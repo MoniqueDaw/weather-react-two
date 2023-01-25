1b355d5398e81bd124fd8a1e22314d4e07bab195
MoniqueDaw <113796650+MoniqueDaw@users.noreply.github.com>
2023-01-25 12:47:21 -0500

    Added Git Repo Url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by Monique D. and is{" "}
+        <a
+          href="https://github.com/MoniqueDaw/weather-react-two"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;

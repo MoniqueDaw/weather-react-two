61f91ac639d8cd996c971c9e199ad9e82d00c981
MoniqueDaw <113796650+MoniqueDaw@users.noreply.github.com>
2023-01-25 22:00:09 -0500

    Wrote the HTML

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
@@ -6,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-        <button className="btn btn-primary">hello</button>
+        <Weather />
         <footer>
           Coded by Monique D. and is{" "}
           <a
